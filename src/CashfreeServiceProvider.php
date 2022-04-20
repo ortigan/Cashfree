f077662a290a5ef92148b9a4da14893786d7bd15
Vinayak.k160 <Vinayak.k160@gmail.com>
2022-04-20 12:48:26 +0530

    updated composer file

--- src/CashfreeServiceProvider.php
+++ src/CashfreeServiceProvider.php
@@ -10,7 +10,7 @@ class CashfreeServiceProvider extends ServiceProvider
         $this->app->bind(Cashfree::class);
         $this->publishes([
             __DIR__.'/../config/cashfree.php' => config_path('cashfree.php'),
-        ]);
+        ], 'ortigan-cashfree-config');
     }
     public function register(){
 
