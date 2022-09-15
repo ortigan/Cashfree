1048f936a5fd0b9db44d57053285ec8d14b489c7
Vinayak.k160 <Vinayak.k160@gmail.com>
2022-09-15 17:10:12 +0530

    added UPI, Bank and IFSC verification

--- src/Payout.php
+++ src/Payout.php
@@ -60,4 +60,29 @@ class Payout extends Request {
         $route = '/payout/v1/requestAsyncTransfer';
         return parent::post($route, $data);
     }
+    public function verifyUPI( array $params )
+    {
+        $route = '/payout/v1/validation/upiDetails' . Util::arrayToParams($params);
+        return parent::get($route);
+    }
+    public function verifyIFSC(string $ifsc)
+    {
+        $route = '/payout/v1/ifsc/' . $ifsc;
+        return parent::get($route);
+    }
+    public function verifyBank( array $params )
+    {
+        $route = '/payout/v1.2/validation/bankDetails' . Util::arrayToParams($params);
+        return parent::get($route);
+    }
+    public function verifyBankAsync( array $params )
+    {
+        $route = '/payout/v1/asyncValidation/bankDetails' . Util::arrayToParams($params);
+        return parent::get($route);
+    }
+    public function verifyBankStatus( string $ref )
+    {
+        $route = '/payout/v1/getValidationStatus/bank?bvRefId=' . $ref;
+        return parent::get($route);
+    }
 }
